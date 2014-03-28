9b428957d11650edfe14097a625f2a700fbf706b
Feather <lisafeather@gmail.com>
2014-03-27 22:13:32 -0700

    still need to figure out how to parse results hash and get form info

--- lib/TwitterStalker/App.pm
+++ lib/TwitterStalker/App.pm
@@ -31,6 +31,19 @@ print "*******************\n";
 my $search_term = "featherart";
 my @tweets = $nt->home_timeline;
 my $r = $nt->search($search_term);
+
+my $doh = $nt->lookup_users({ screen_name => 'featherart,hansflorine' });
+#only prints the users we have in common
+print Dumper $doh;
+
+# this might be a way to at least look at the hash!
+# $Data::Dumper::Sortkeys = \&my_filter;
+# my $foo = { map { (ord, "$_$_$_") } 'I'..'Q' };
+# my $bar = { %$foo };
+# my $baz = { reverse %$foo };
+# print Dumper [ $foo, $bar, $baz ];
+
+
 #   print Dumper $r;
 #my %friends = $nt->friends;
 #print $nt;
@@ -40,6 +53,9 @@ my $r = $nt->search($search_term);
 #print "$tweets[0]\n";
 #print $res;
 print "@@@@@@@@@@@@@@@@@@@\n";
+print $r;
+my %data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
 
 get '/' => sub {
     # template 'index';
@@ -49,6 +65,11 @@ get '/' => sub {
 get '/find-tweets/:name' => sub {
   my $name = params->{name};
   my $response = $nt->search($name);
+
+# while ( ($key, $value) = each %hash )
+# {
+#   print "key: $key, value: $hash{$key}\n";
+# }
 
   # Dancer adds .tt automatically, but this is configurable
   template 'hello' => {
