ec853f971debcc55f0513740f5ec8865bc7d5f48
Feather <lisafeather@gmail.com>
2014-03-28 15:28:53 -0700

    huge sigh

--- lib/TwitterStalker/App.pm
+++ lib/TwitterStalker/App.pm
@@ -3,6 +3,7 @@ use Dancer ':syntax';
 use Net::Twitter::Lite::WithAPIv1_1;
 use Scalar::Util 'blessed';
 use Data::Dumper;
+use Template;
 
 our $VERSION = '0.1';
 my $consumer_key = $ENV{'TWITTER_KEY'};
@@ -23,26 +24,16 @@ my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 #$nt->update('Hello World!');
 
 print "*******************\n";
-# my $doh = $nt->lookup_users({ screen_name => 'featherart,hansflorine' });
-# #only prints the users we have in common
-# print Dumper $doh;
-
+#my $doh = $nt->lookup_users({ screen_name => 'featherart,hansflorine' });
+#only prints the users we have in common
+#print Dumper $doh;
+   
 # this might be a way to at least look at the hash!
 # $Data::Dumper::Sortkeys = \&my_filter;
 # my $foo = { map { (ord, "$_$_$_") } 'I'..'Q' };
 # my $bar = { %$foo };
 # my $baz = { reverse %$foo };
 # print Dumper [ $foo, $bar, $baz ];
-
-
-#   print Dumper $r;
-#my %friends = $nt->friends;
-#print $nt;
-# print @tweets[0];
-# print "\n";
-#print %friends[0];
-#print "$tweets[0]\n";
-#print $res;
 
 print "@@@@@@@@@@@@@@@@@@@\n";
 get '/' => sub {
@@ -51,24 +42,32 @@ get '/' => sub {
 };
 
 get '/find_tweets/:name' => sub {
+#get '/find_tweets' => sub {
   # nope
-  # my $form = form('find_tweets');
-  my $name = params->{name};
-  my $response = $nt->search($name);
+  #my $form = form('find_tweets');
 
-  # Dancer adds .tt automatically, but this is configurable
+
+  my $name = params->{name};
+
+  my $r = eval { $nt->search($name) };
+    # for my $status ( @{$r->{statuses}} ) {
+    #     print "$status->{text}\n";
+    # }
+
+  set template => 'template_toolkit';
+  
   template 'find_tweets' => {
       name => $name,
-      response => $response
-  };
+      response => $r
+    };
 };
 
+# not the right way to do this, probably
 get '/user_results/:name1&:name2' => sub {
   my $name1 = params->{name1};
   my $name2 = params->{name2};
   my $response = $nt->lookup_users({ screen_name => $name1,$name2 });
 
-  # Dancer adds .tt automatically, but this is configurable
   template 'user_results' => {
       name1 => $name1,
       name2 => $name2,
@@ -81,17 +80,11 @@ print "@++++++++++++++++@@@\n";
 get '/hello/:name' => sub {
   my $name = params->{name};
 
-  # Dancer adds .tt automatically, but this is configurable
   template 'hello' => {
       name => $name,
   };
 };
 
-# get '/tweets' => sub {
-#   my $form = form('search_tweets');
-#   print "here's my form: \n";
-#   print $form;
-# };
 # print "@++++++++++++++++@@@\n";
 # post '/tweets' => sub {
 #   my $form = form('search_tweets');
