7cbc40a85529d4d5055b953a9130ff981f19fed2
Feather <lisafeather@gmail.com>
2014-03-27 21:17:10 -0700

    got variables rendering in a template, booya!

--- lib/TwitterStalker/App.pm
+++ lib/TwitterStalker/App.pm
@@ -24,16 +24,16 @@ my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 #$nt->update('Hello World!');
 
 print "*******************\n";
-print "twitter_secret:      ", $ENV{'TWITTER_KEY'}, "<BR>", "\n";
-print "hello?:      ", $ENV{'LANG'}, "<BR>", "\n";
-print $consumer_key, "\n";
+# print "twitter_secret:      ", $ENV{'TWITTER_KEY'}, "<BR>", "\n";
+# print "hello?:      ", $ENV{'LANG'}, "<BR>", "\n";
+# print $consumer_key, "\n";
 
 my $search_term = "featherart";
 my @tweets = $nt->home_timeline;
 my $r = $nt->search($search_term);
-    print Dumper $r;
+#   print Dumper $r;
 #my %friends = $nt->friends;
-print $nt;
+#print $nt;
 # print @tweets[0];
 # print "\n";
 #print %friends[0];
@@ -46,14 +46,39 @@ get '/' => sub {
     template 'hello';
 };
 
-get '/checkout' => sub {
-        my $form;
 
-        $form = form('checkout');
-        
-        template 'checkout', {form => $form};
-    };
+print "@++++++++++++++++@@@\n";
+#get '/user/view/:username/' => sub {
+# get '/hello/:name' => sub {
+#         #return "Why, hello there " . params->{name};
+#         my $name = params->{name};
+#         print $name;
+#         template 'hello' => { name => $name };
+# };
 
+get '/hello/:name' => sub {
+  my $name = params->{name};
+
+  # Dancer adds .tt automatically, but this is configurable
+  template 'hello' => {
+      name => $name,
+  };
+};
+# get '/tweets' => sub {
+#   my $form = form('search_tweets');
+#   print "here's my form: \n";
+#   print $form;
+
+# };
+print "@@@@@@@@@@@@@@@@@@@\n";
+print "@++++++++++++++++@@@\n";
+
+# post '/tweets' => sub {
+#   my $form = form('search_tweets');
+#   my $values = $form->values();
+#   print "here's my form vals: \n";
+#   print $values;
+
+# };
+print "@@@@@@@@@@@@@@@@@@@\n";
 true;
-
-
