8653c82447d011043f4f844736669cb4e8326be2
Feather <lisafeather@gmail.com>
2014-03-27 18:06:05 -0700

    got oauth handshake working now need to figure out views and how to get specific info for a user

--- lib/TwitterStalker/App.pm
+++ lib/TwitterStalker/App.pm
@@ -1,14 +1,51 @@
-package TwitterStalker::App;
+package MyWeb::App;
 use Dancer ':syntax';
+use Net::Twitter::Lite::WithAPIv1_1;
+use Scalar::Util 'blessed';
+use Data::Dumper;
 
 our $VERSION = '0.1';
+my $consumer_key = $ENV{'TWITTER_KEY'};
+my $consumer_secret = $ENV{'TWITTER_SECRET'};
+my $access_token = $ENV{'TWITTER_TOKEN'};
+my $access_token_secret = $ENV{'TWITTER_TOKEN_SECRET'};
 
-get '/' => sub {
-    template 'index';
-};
 
-get '/hello' => sub {
-    template 'hello';
-};
+my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
+      consumer_key        => $consumer_key,
+      consumer_secret     => $consumer_secret,
+      access_token        => $access_token,
+      access_token_secret => $access_token_secret,
+      ssl                 => 1,
+);
+# this causes 403
+#my $res    = $nt->update({ status => "I CAN HAZ OAUTH!" });
+# but this works SO BE CAREFUL
+#$nt->update('Hello World!');
+
+print "*******************\n";
+print "twitter_secret:      ", $ENV{'TWITTER_KEY'}, "<BR>", "\n";
+print "hello?:      ", $ENV{'LANG'}, "<BR>", "\n";
+print $consumer_key, "\n";
+
+my $search_term = "featherart";
+my @tweets = $nt->home_timeline;
+my $r = $nt->search($search_term);
+    print Dumper $r;
+#my %friends = $nt->friends;
+print $nt;
+# print @tweets[0];
+# print "\n";
+#print %friends[0];
+#print "$tweets[0]\n";
+#print $res;
+print "@@@@@@@@@@@@@@@@@@@\n";
+
+# get '/' => sub {
+#     # template 'index';
+#     template 'hello';
+# };
 
 true;
+
+
